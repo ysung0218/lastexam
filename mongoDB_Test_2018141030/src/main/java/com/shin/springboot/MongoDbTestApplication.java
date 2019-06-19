9e5a5f119ded5d50ba6ebc996fba734a919ea788
ysung0218 <ysung0218@gmail.com>
2019-06-20 03:27:57 +0900

    MongoDbTestApplication

---
+++ mongoDB_Test_2018141030/src/main/java/com/shin/springboot/MongoDbTestApplication.java
@@ -0,0 +1,13 @@
+package com.shin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbTestApplication.class, args);
+	}
+
+}
