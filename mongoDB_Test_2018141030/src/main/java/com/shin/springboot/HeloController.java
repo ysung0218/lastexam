9f73200843d806dacabcb6fb2d7bed1f93cdcd4d
ysung0218 <ysung0218@gmail.com>
2019-06-20 03:36:12 +0900

    HeloController.java 메인페이지 설정 작성

---
+++ mongoDB_Test_2018141030/src/main/java/com/shin/springboot/HeloController.java
@@ -0,0 +1,49 @@
+package com.shin.springboot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shin.springboot.repositories.MyDataMongoRepository;
+
+@Controller
+public class HeloController {
+
+	@Autowired
+	MyDataMongoRepository repository;
+	
+	/**
+	 *
+	 * @fn 		public ModelAndView index(ModelAndView mav)
+	 * 
+	 * @brief 	메인 페이지 설정 
+	 *
+	 * @author 	신예성
+	 * @date 	2019-06-20
+	 *
+	 * @param 	mav ModelAndView
+	 *
+	 * @remark 	웹 페이지를 불러오기 위한 초기페이지 설정		[2019-06-20; 신예성] \n
+	 * 		   	FinaAll()를 이용해 list에 모든 데이터를 저장	[2019-06-20; 신예성] \n
+	 *
+	 */
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+
+		mav.setViewName("index");
+		mav.addObject("title", "여행 일정 작성 페이지");
+		mav.addObject("msg", "일정을 입력해주세요.");
+
+		Iterable<MyDataMongo> list = repository.findAll();
+		mav.addObject("datalist", list);
+		return mav;
+	}
+
+}
